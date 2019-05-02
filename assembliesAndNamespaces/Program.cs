e416ebd92d6a945b13018adad1fcf3294b4d9ca3
Sander Buruma <sanderburuma@gmail.com>
2019-05-02 16:53:56 +0200

    saving to file of string and website string

---
+++ assembliesAndNamespaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace assembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("https://www.barnhardt.biz/");
+            string text = "A class is the most powerful data type in C#. Like a structure " +
+                "a class defines the data behavior of the program";
+
+            File.WriteAllText(@"C:\Users\Cubit32\Desktop\coding\ITVitae\assembliesAndNamespaces\writeText.txt", text);
+            File.WriteAllText(@"C:\Users\Cubit32\Desktop\coding\ITVitae\assembliesAndNamespaces\website.txt", reply);
+
+            Console.WriteLine("Hello moon!");
+            Console.ReadLine();
+        }
+    }
+}

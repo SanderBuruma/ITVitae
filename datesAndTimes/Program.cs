cbc2c334da12350dc31fa18199bd742701588c47
Sander Buruma <sanderburuma@gmail.com>
2019-05-02 12:28:11 +0200

    added multiplesof3or5 and dateTime projects

---
+++ datesAndTimes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValye = DateTime.Now;
+            //Console.WriteLine(myValye.ToShortDateString());
+            //Console.WriteLine(myValye.ToShortTimeString());
+            //Console.WriteLine(myValye.ToLongDateString());
+            //Console.WriteLine(myValye.ToLongTimeString());
+            //Console.WriteLine(myValye.AddDays(3).ToShortDateString());
+
+            DateTime myBirthDay = new DateTime(1987, 7, 3, 23, 59, 59);
+            Console.WriteLine(myBirthDay.ToLongDateString());
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthDay);
+            Console.WriteLine(myAge.TotalDays);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sumMultiplesOf3or5/sumMultiplesOf3and5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace sumMultiplesOf3and5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            while(run)
+            {
+                Console.Clear();
+                Console.WriteLine("Options: ");
+                Console.WriteLine("1) Get the sum of all integers that are multiples of 3 or 5 equal to or less than X.");
+                Console.WriteLine("2) Exit Program");
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    Console.Write("Positive Integer: ");
+                    int x;
+                    if (Int32.TryParse(Console.ReadLine(), out x))
+                    {
+                        sumOfMultiplesOf3or5(x);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter an integer to make a correct choice");
+                    }
+                    Console.WriteLine("\nPress enter to continue");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    run = false;
+                }
+
+            }
+        }
+
+        private static void sumOfMultiplesOf3or5(int x)
+        {
+            if (x > 0)
+            {
+                if (x < 1000001)
+                {
+                    int sum = 0;
+                    while (x > 0)
+                    {
+                        if (x % 3 == 0 || x % 5 == 0)
+                        {
+                            sum += x;
+                        }
+                        x--;
+                    }
+                    Console.WriteLine(String.Format("sum: {0}", sum));
+                }
+                else Console.WriteLine("Integer may not be larger than 1,000,000");
+            }
+            else Console.WriteLine("Integer may not be smaller than 1");
+        }
+    }
+}

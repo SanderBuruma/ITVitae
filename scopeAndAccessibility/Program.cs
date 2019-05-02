aeb72131e68086654b8692cb7073e529ad886e27
Sander Buruma <sanderburuma@gmail.com>
2019-05-02 15:44:02 +0200

    made an example dealing with scope

---
+++ scopeAndAccessibility/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scopeAndAccessibility
+{
+    class Program
+    {
+        private static string k = "";
+
+        static void Main(string[] args)
+        {
+            string j = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                j = i.ToString();
+                k = i.ToString();
+                Console.WriteLine(j);
+            }
+            Console.WriteLine("Outside of the for: " + j);
+            Console.WriteLine("Outside of the for: " + k);
+            HelperMethod();
+
+            Car car = new Car("Toyota", "Truck", 1967, "Blue");
+            car.DoSomething();
+
+            Console.ReadLine();
+        }
+
+        static void HelperMethod()
+        {
+            Console.WriteLine("Value of k from the HelperMethod(): " + k);
+        }
+
+        class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+            public string Color { get; set; }
+
+            public Car(string make, string model, int year, string color)
+            {
+                Make = make;
+                Model = model;
+                Year = year;
+                Color = color;
+            }
+            public void DoSomething()
+            {
+                Console.WriteLine(this.HelperMethod());
+            }
+            private string HelperMethod()
+            {
+                return "Care are awesome!";
+            }
+        }
+    }
+}

55ccc0044dfd635af9cc243993ee7c81af32d020
Sander Buruma <sanderburuma@gmail.com>
2019-05-02 12:56:15 +0200

    cars, working with classes and methods

---
+++ cars1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cars1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Truck";
+            myCar.Year = 2020;
+            myCar.Color = "Light Grey";
+
+            Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color);
+
+            Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
+
+            Console.ReadLine();
+        }
+
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 100.00m;
+
+            return carValue;
+
+        }
+
+        class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+            public string Color { get; set; }
+
+            public decimal DetermineMarketValue()
+            {
+                decimal carValue;
+                if (Year > 1990) carValue = 10000;
+                else carValue = 2000;
+                return carValue;
+            }
+        }
+    }
+}

eb9aec8bf2a7b8a21eb23456702a5f6c7b324e13
Sander Buruma <sanderburuma@gmail.com>
2019-05-02 21:06:27 +0200

    exception handling

---
+++ HandlingExceptions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Users\Cubit32\desktop\coding\ITVitae\HandlingExceptions\example.txt");
+                Console.WriteLine(content);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the file is correct.");
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the path to the directory is correct.");
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Closing application now...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
